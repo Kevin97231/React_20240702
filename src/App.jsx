604aad8917c9bf96ef3941f544fe4ffa4c19aae7
klabarre <klabarre@dawan.fr>
2024-07-02 16:51:32 +0200

    modif navBar + ajout de la vue 'HookUseState.jsx'

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import ExempleJsx from "./components/view/ExempleJsx";
 import Navbar from "./components/Navbar";
+import { HookUseState } from "./components/view/HookUseState";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<ExempleJsx />} />
-              <Route path="/test" element={<>TEST</>} />
+              <Route path="/use-state" element={<HookUseState />} />
             </Routes>
           </BrowserRouter>
         </div>
---
+++ src/components/view/HookUseState.jsx
@@ -0,0 +1,7 @@
+export const HookUseState = () => {
+  return (
+    <>
+      <h1>Le hook useState</h1>
+    </>
+  );
+};
